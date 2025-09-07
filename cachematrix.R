483f1697bdfb58ea168152b222096f1fe22c14f0
samitkumardas321@gmail.com <samitkumardas321@gmail.com>
2025-09-07 12:39:05 +0530

    Latest commit for CacheMatrix assignment for John Hopkin's R programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The makeCacheMatrix function takes a matrix as an input
+## The set function assigns values to the x variable (to the eclosing env)
+## The get fucntion retrieves the stored value of x
+## The setInv function contains the inverse value of x coming from cacheSolve funnction.
+## The getInv function fetches the inverse value of x.
+
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -20,7 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+## The cacheSolve function returns the already stored inverse matrix if x not updated
+## If i doesnot have a value then,
+## 1) The value of x will be fethed from the instance of makeCacheMatrix function
+## 2) The inverse matrix of x will be calculated and stored in i
+## 3) The i value will be stored to the instance and can be retrived-
+##    -using the getInv function.
+## Note: the cacheSolve fucntion expects a squaure matrix to compute the-
+##       -inverse otherwise, it will return an error.
 
 cacheSolve <- function(x, ...) {
   i <- x$getInv()
